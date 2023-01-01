89c8e1eac1b56b9c3bdf61e619a863b916e7f30a
tharuAnish <namoanishtharu@gmail.com>
2023-01-01 20:55:11 +0530

    navbar components added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Create from "./pages/Create/Create"
 import Search from "./pages/Search/Search"
 import Recepie from "./pages/Recepie/Recepie"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link></Link>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
