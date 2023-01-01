2008fa1bafe3c66c220ec1ee11affc7fb54cdf0b
tharuAnish <namoanishtharu@gmail.com>
2023-01-01 21:17:46 +0530

    fetching data with usefetch hook

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,19 @@
 import "./Home.css"
+import { useFetch } from "../../hooks/useFetch"
 
 export default function Home() {
+  const { data, isLoading, error } = useFetch("http://localhost:3000/recipes")
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {/*if only we have data then only it will fetch */}
+      {data &&
+        data.map((recepie) => (
+          <div key={recepie.id}>
+            <h2>{recepie.title}</h2>
+          </div>
+        ))}
     </div>
   )
 }
