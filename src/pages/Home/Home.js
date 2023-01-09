14b00ee19191597a46478a375b308b91ded79e94
tharuAnish <namoanishtharu@gmail.com>
2023-01-09 21:05:27 +0530

    10.1 - making a post request to add a new recipe to json data server

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,17 @@
-import "./Home.css"
 import { useFetch } from "../../hooks/useFetch"
+import RecipeList from "../../components/RecipeList/RecipeList"
+
+// styles
+import "./Home.css"
 
 export default function Home() {
-  const { data, isLoading, error } = useFetch("http://localhost:3000/recipes")
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
+
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
-      {isLoading && <p className="loading">Loading...</p>}
-      {/*if only we have data then only it will fetch */}
-      {data &&
-        data.map((recepie) => (
-          <div key={recepie.id}>
-            <h2>{recepie.title}</h2>
-          </div>
-        ))}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
--- src/pages/Recepie/Recepie.js
+++
@@ -1,9 +0,0 @@
-import "./Recepie"
-
-export default function Recepie() {
-  return (
-    <div>
-      <h1>Recepie</h1>
-    </div>
-  )
-}
