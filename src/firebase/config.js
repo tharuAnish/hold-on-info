befc6808f66edd62dac84b7e0b27d6ece6c94603
tharuAnish <namoanishtharu@gmail.com>
2023-04-19 10:18:46 +0530

    firebase config file added and initialized

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl46kJ2buDiVRe1jbv94P7nHVQROI4NUQ",
+  authDomain: "cooking-ninja-site-6b9d1.firebaseapp.com",
+  projectId: "cooking-ninja-site-6b9d1",
+  storageBucket: "cooking-ninja-site-6b9d1.appspot.com",
+  messagingSenderId: "385591906182",
+  appId: "1:385591906182:web:acf71928175b2cbd45e607",
+}
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services
+const projectFirestore = firebase.firestore()
