7a3c8ed45bee9979d29ce1c5f1d4c49cc07b26cb
tharuAnish <namoanishtharu@gmail.com>
2023-04-19 13:10:00 +0530

    fetching a firestore collection

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,42 @@
-import { useFetch } from "../../hooks/useFetch"
+import { projectFirestore } from "../../firebase/config"
 import RecipeList from "../../components/RecipeList/RecipeList"
-
 // styles
 import "./Home.css"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
-
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    setIsPending(true) // because from this point we are going to start fetching the data
+    //reach out the firestore to grab all the recepies from that collection
+    projectFirestore
+      .collection("recipes")
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot)
+        // as it is asynchronous we use get then // as all the data from recipe collection is stored in snapshot
+        //now check is the snapshot object empty
+        if (snapshot.empty) {
+          setError("No Recipes to load") //if is empty
+          setIsPending(false)
+        } else {
+          // firestore is not empty
+          let results = []
+          snapshot.docs.forEach((doc) => {
+            // console.log(doc)
+            results.push({ id: doc.id, ...doc.data() })
+          }) // loop all data with for each //in colsole we saw the snapshot property docs contains array of data
+          setData(results)
+          setIsPending(false)
+        }
+      })
+      .catch((err) => {
+        setError(err.message)
+        setIsPending(false)
+      })
+  }, [])
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
