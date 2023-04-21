bca58d6c82e3c8cc6518fec95308001105a830a8
tharuAnish <namoanishtharu@gmail.com>
2023-04-21 19:33:16 +0530

    firebase add delete and realtime

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom"
+import Trashcan from "../../assets/trash-can.svg"
 
 // styles
 import "./RecipeList.css"
 import { useTheme } from "../../hooks/useTheme"
+import { projectFirestore } from "../../firebase/config"
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme() //mode imported
   if (recipes.length === 0) {
     return <div className="error">No recipe to Load...</div>
   }
+
+  const handleClick = (id) => {
+    projectFirestore.collection("recipes").doc(id).delete()
+  } //firebase delete
 
   return (
     <div className="recipe-list">
@@ -18,6 +24,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className="delete"
+            src={Trashcan}
+            onClick={() => handleClick(recipe.id)}
+            // we are again invoking so that it doesnot automatically fires
+          />
         </div>
       ))}
     </div>
